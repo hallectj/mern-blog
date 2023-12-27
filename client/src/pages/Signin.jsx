5f67c19a9ec94324d3887a99881c4c57262c6428
Travis Halleck <hallectj@gmail.com>
2023-12-27 02:00:04 -0600

    complete sign in page functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,7 +1,95 @@
-import React from 'react'
+import { Alert, Button, Label, TextInput, Spinner } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
-export default function Signin() {
+export default function SignIn() {
+  const [formData, setFormData] = useState({})
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate()
+
+  let [loading, setLoading] = useState(false);
+
+  const handleChange = (event) => {
+    setFormData({...formData, [event.target.id]: event.target.value.trim()});
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if(!formData.password || !formData.email){
+      return setErrorMessage("Please fill out all fields");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const response = await fetch('./api/auth/signin', {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData)
+      });
+      const data = await response.json();
+      if(data.success === false){
+        setLoading(false);
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(response.ok){
+        navigate('/');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false)
+    }
+  }
+
   return (
-    <div>Signin</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/** left */}
+        <div className='flex-1'>
+          <Link to="/" className='font-semibold dark:text-white text-4xl'>
+            <span className='px-2 py-1 rounded-lg text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium text-center'>Travis's</span>
+            Blog
+          </Link>
+          <p className='text-sm mt-5'>
+            This is a demo project. You can sign in with your email and password or with Google.
+          </p>
+        </div>
+
+        {/** right */}
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div>
+              <Label value="Your email" />
+              <TextInput type='email' placeholder='name@example.com' id='email' onChange={handleChange} />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput type='password' placeholder='**********' id='password' onChange={handleChange} />
+            </div>
+            <Button className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm text-center' type='submit' disabled={loading}>
+            {
+                loading ? (
+                  <>
+                    <Spinner size="sm" />
+                    <span className='pl-2'>Loading</span>
+                  </>
+                ) : 'Sign In'
+              }
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Don't have an account?</span>
+            <Link to="/sign-up" className='text-blue-500'>Sign Up</Link>
+          </div>
+          {
+            errorMessage && (
+              <Alert className='mt-5' color="failure">
+                {errorMessage}
+              </Alert>
+            )
+          }
+        </div>
+      </div>
+    </div>
   )
 }
