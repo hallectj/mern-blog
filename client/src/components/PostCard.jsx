f21a3660484641357dabee23e2d8a92f2efdf424
Travis Halleck <hallectj@gmail.com>
2024-01-07 08:36:36 -0600

    Made posts clickable bug fix and made homepage text bigger

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -48,26 +48,26 @@ export default function PostCard({post}) {
 
   return (
     <div className="flex flex-col items-center h-auto min-w-[300px] flex-1">
-      <div className="bg-white rounded-lg shadow-lg overflow-hidden max-w-lg w-full">
-        <img src={`${post.image}`} alt="Mountain" className="object-fill mt-10 p-3 h-[260px] w-full" />
-        <div className="p-3">
-          <div className='mb-2 h-[60px]'>
-            <Link to={`/post/${post.slug}`}>
-                <h2 className="text-xl text-center font-bold text-gray-800">{post.title}</h2>
-            </Link>
-          </div>
-          <div className='pb-1 h-[100px]'>
-            <p className="text-gray-700 leading-tight mb-4">{extractedDesc}</p>
-          </div>
-          <div className="flex justify-between items-center">
-            <div className="flex items-center">
-              <img src={postUser.photoURL} alt="Avatar" className="w-8 h-8 rounded-full mr-2 object-cover" />
-                <span className="text-gray-800 font-semibold">{postUser.username}</span>
+      <Link to={`/post/${post.slug}`} as="div">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden max-w-lg w-full">
+          <img src={`${post.image}`} alt="Mountain" className="object-fill mt-10 p-3 h-[260px] w-full" />
+          <div className="p-3">
+            <div className='mb-2 h-[60px]'>
+              <h2 className="text-xl text-center font-bold text-gray-800">{post.title}</h2>
             </div>
-            <span className="text-gray-600">{moment(post.updatedAt).fromNow()}</span>
+            <div className='pb-1 h-[100px]'>
+              <p className="text-gray-700 leading-tight mb-4">{extractedDesc}</p>
+            </div>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center">
+                <img src={postUser.photoURL} alt="Avatar" className="w-8 h-8 rounded-full mr-2 object-cover" />
+                  <span className="text-gray-800 font-semibold">{postUser.username}</span>
+              </div>
+              <span className="text-gray-600">{moment(post.updatedAt).fromNow()}</span>
+            </div>
           </div>
-        </div>
-      </div>
+        </div>  
+      </Link>
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,21 +24,23 @@ export default function Home() {
 
   return (
     <div>
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto text-center'>
         <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
-        <p className='text-gray-500 text-xs sm:text-sm'>This is a test website built with React, Redux, MongoDb, NodeJS and Express. Articles are largely dummy articles.</p>
+        <p className='text-gray-500 sm:text-sm md:text-xl'>This is a test website built with <b>React, Redux, MongoDb, NodeJS and Express</b>. Articles are largely dummy articles. This website features authentication via traditional means by creating an account or using Google Auth. Once inside, if the user is an admin, they have the ability to create, edit, update and delete posts. Admins also have the ability to delete comments from regular users. In addition to everything mentioned, admins also have a dashboard section where they can see all the comments, see members and see all the posts admins have posted.</p>
         <Link to='/search' className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'>
           View All Articles
         </Link>
       </div>
 
-      <div className='p-3 bg-amber-100 dark:bg-slate-700 ml-5 mr-5'>
-        <CallToAction
-          imgSrc={ReactLogo}
-          heading='Ad Heading Would Go Here'
-          paragraph='Call to Action paragraph would go here.'
-          link='https://www.google.com'
-        />
+      <div className='mx-auto flex justify-center w-full'>
+        <div className='p-3 bg-amber-100 dark:bg-slate-700 ml-5 mr-5 max-w-3xl'>
+          <CallToAction
+            imgSrc={ReactLogo}
+            heading='Ad Heading Would Go Here'
+            paragraph='Call to Action paragraph would go here.'
+            link='https://www.google.com'
+          />
+        </div>
       </div>
 
       <div className='flex flex-col gap-8 max-w-6xl mx-auto p-3 py-7'>
